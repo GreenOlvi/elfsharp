07348c965f58dc4cb79b0966463f7cf804bbb9d3
Konrad M. Kruczyński <konrad.kruczynski@gmail.com>
2015-03-13 23:18:18 +0100

    First code for the Mach-O support.

---
+++ Tests/MachO/OpeningTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using ELFSharp.MachO;
+
+namespace Tests.MachO
+{
+    [TestFixture]
+    public class OpeningTests
+    {
+        [Test]
+        public void ShouldOpenMachO()
+        {
+            var fileName = Utilities.GetBinaryLocation("simple-mach-o");
+            ELFSharp.MachO.MachO machO;
+            Assert.AreEqual(MachOResult.OK, MachOReader.TryLoad(fileName, out machO));
+        }
+
+        [Test]
+        public void ShouldFindCorrectMachine()
+        {
+            var fileName = Utilities.GetBinaryLocation("simple-mach-o");
+            var machO = MachOReader.Load(fileName);
+            Assert.AreEqual(Machine.X86_64, machO.Machine);
+        }
+    }
+}
+
